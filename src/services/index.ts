a0c65740b62b5bb45666108782d41710a5588699
Ian Mukua <ianmukuaa@gmail.com>
2025-04-24 21:43:54 +0300

    ADD: Create index file for service exports, including authService and commented-out services

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+//export {default as userService} from './user.service';
+//export {default as clientService} from './client.service';
+export { default as authService } from './auth.service';
+//export {default as programService} from './program.service';
+//export {default as enrollmentervice} from './enrollment.service';
