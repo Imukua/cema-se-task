b9a921abf1270478b8192852cb2143cc4919f65e
Ian Mukua <ianmukuaa@gmail.com>
2025-04-24 13:37:43 +0300

    ADD: Express application setup with middleware for logging, compression, and CORS

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
+import config from './config/config';
+import morgan from './config/morgan';
+
+
+const app = express();
+
+if (config.env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
+
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(compression());
+
+app.use(cors());
+app.options('*', cors());
+
+
+
+export default app;
