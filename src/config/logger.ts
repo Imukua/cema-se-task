6959c202695e83a770605a280752791f1e1cea80
Ian Mukua <ianmukuaa@gmail.com>
2025-04-24 13:20:13 +0300

    ADD: Logger configuration using Winston for  application-level logs

---
+++ src/config/logger.ts
@@ -0,0 +1,46 @@
+import winston from 'winston';
+import path from 'path';
+import config from './config';
+
+
+const logDirectory = '/tmp' 
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const logger = winston.createLogger({
+  level: config.env === 'development' ? 'debug' : 'info',
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    config.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.splat(),
+    winston.format.printf(({ level, message, timestamp, ...metadata }) => {
+      let msg = `${timestamp} [${level}]: ${message}`;
+      if (Object.keys(metadata).length > 0) {
+        msg += JSON.stringify(metadata, null, 2);
+      }
+      return msg;
+    })
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ['error']
+    }),
+    new winston.transports.File({
+      filename: path.join(logDirectory, 'error.log'),
+      level: 'error'
+    }),
+    new winston.transports.File({
+      filename: path.join(logDirectory, 'combined.log')
+    })
+  ]
+});
+
+export default logger;
