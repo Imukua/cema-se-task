7324d232703b032de15eabe4d61cb96b392fb798
Ian Mukua <ianmukuaa@gmail.com>
2025-04-24 20:54:00 +0300

    REFACTOR: ModifIid UserSchema to allow nullable role and add password field with minimum length requirement

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -5,21 +5,24 @@ export const UserSchema = z.object({
   id: z.string().uuid(),
   name: z.string(),
   email: z.string().email(),
-  role: z.nativeEnum(UserRole, { message: 'Role must be either admin, seller, or buyer' }),
+  role: z.nativeEnum(UserRole, { message: 'Role must be either ADMIN or USER' }).nullable(),
   createdAt: z.date(),
-  updatedAt: z.date()
+  updatedAt: z.date(),
+  password: z.string().min(8)
 });
 
 export const UserCreateSchema = UserSchema.omit({
   id: true,
   createdAt: true,
-  updatedAt: true
+  updatedAt: true,
+  role: true
 });
 
 export const UserUpdateSchema = UserSchema.partial().omit({
   id: true,
   createdAt: true,
-  updatedAt: true
+  updatedAt: true,
+  role: true
 });
 
 export type User = z.infer<typeof UserSchema>;
