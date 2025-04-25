7ccf2e9e82f60c598aa4fad387e7755c8683c250
Ian Mukua <ianmukuaa@gmail.com>
2025-04-25 13:09:21 +0300

    UPDATE: Enhance login route by adding validation with userLoginSchema for improved credential verification

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import { authController } from '../../controllers';
 import validate from '../../middleware/validate';
-import { UserCreateSchema } from '../../types/user.types';
+import { UserCreateSchema, userLoginSchema } from '../../types/user.types';
 
 const router = express.Router();
 
 router.post('/register', validate(UserCreateSchema), authController.register);
 
-router.post('/login', authController.login);
+router.post('/login', validate(userLoginSchema), authController.login);
 
 router.post('/refresh-tokens', authController.refreshTokens);
 
