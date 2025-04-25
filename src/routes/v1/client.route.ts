2d793db6a70cdc2176ff6fd70c26a4bd5edd9098
Ian Mukua <ianmukuaa@gmail.com>
2025-04-25 17:40:28 +0300

    UPDATE: Integrate validation middleware for client creation and update routes to enhance data integrity and error handling

--- src/routes/v1/client.route.ts
+++ src/routes/v1/client.route.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import { clientController } from '../../controllers';
+import auth from '../../middleware/auth';
+import validate from '../../middleware/validate';
+import { ClientCreateSchema, ClientUpdateSchema } from '../../types/client.types';
 
 const router = express.Router();
+router.use(auth());
 
-router.post('/', clientController.createClient);
+router.post('/', validate(ClientCreateSchema), clientController.createClient);
 
 router.get('/', clientController.searchClients);
 
 router.get('/:clientId', clientController.getClientProfile);
 
-router.patch('/:clientId', clientController.updateClient);
+router.patch('/:clientId', validate(ClientUpdateSchema), clientController.updateClient);
 
 router.delete('/:clientId', clientController.deleteClient);
 
