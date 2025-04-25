c19a48cea52d06512aaae65e7cd99b1995506496
Ian Mukua <ianmukuaa@gmail.com>
2025-04-25 16:17:44 +0300

    UPDATE: Enable enrollment service export in index file for improved service accessibility

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,4 +2,4 @@ export { default as userService } from './user.serviice';
 //export {default as clientService} from './client.service';
 export { default as authService } from './auth.service';
 //export {default as programService} from './program.service';
-//export {default as enrollmentervice} from './enrollment.service';
+export { default as enrollmentService } from './enrollment.service';
